8aeca9fedc694b47ebec52db2504b6cbade6a87e
jcamhi <jeremshy@h3lp.fr>
2017-12-22 01:11:58 +0100

    debug

--- logic.py
+++ logic.py
@@ -7,29 +7,36 @@ def exec_operand(stack, operand):
 	val1 = stack.pop()
 	val2 = stack.pop()
 	if (operand == '+'):
-		if (val1 == -1 and val2 == -1):
+		if (val1 == -2 or val2 == -2);
+			stack.append(-2)
+		elif (val1 == -1 or val2 == -1)
 			stack.append(-1)
-		elif (val1 == 0 or val2 == 0):
+		elif (val1 == 1 and val2 == 1):
+			stack.append(1)
+		else
 			stack.append(0)
-		elif (val1 == -1 or val2 == -1):
-			stack.append(-1)
-		else:
-			stack.append(1)
+
 	elif (operand == '|'):
-		if (val1 == -1 and val2 == -1):
-			stack.append(-1)
-		elif (val1 == 1 or val2 == 1):
+		if (val1 == 1 or val2 == 1)
 			stack.append(1)
-		elif (val1 == -1 or val2 == -1):
+		elif (val1 == -2 or val2 == -2);
+			stack.append(-2)
+		elif (val1 == -1 or val2 == -1)
 			stack.append(-1)
-		else:
+		else
 			stack.append(0)
 	elif (operand == '^'):
-		if (val1 == val2):
+		if (val1 == -2 or val2 == -2);
+			stack.append(-2)
+		elif (val1 == 1 and val2 == 1)
 			stack.append(0)
-		else:
+		elif (val1 == 1 or val2 == 1)
 			stack.append(1)
-		pass
+
+=B
+B ^ C => C
+
+
 
 def is_true(expression):
 	stack = []
@@ -60,12 +67,18 @@ def exec_as_true(expression):
 			return (changes)
 		if (is_fact(expression[0])):
 			factval = dico[expression[0]];
-			if (factval == 0):
-				raise LogicError("Error")
 			if (dico[expression[0]] != 1):
 				changes = True
 				print expression[0] + " devient vrai"
 			dico[expression[0]] = 1
-		elif (expression[0] != '+'):
-			raise ParseException("Invalid conclusion")
 		expression = expression[1:]
+
+def is_true_with_dico(expression, other):
+	global dico
+	save = dico
+	dico = other
+	is_true(expression)
+	dico = save
+
+def find_all_possibilities(expression):
+	dico_list = []
